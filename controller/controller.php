28791651f60d7d3d2bfdca970168d57846a3c2f2
scarlett <scarlettkimm@gmail.com>
2020-04-28 15:06:03 -0400

    add changes

--- controller/controller.php
+++ controller/controller.php
@@ -1,33 +1,18 @@
 <?php
 class Controller {
-    /**
-     * @var Base
-     */
+
     private $_f3;
-    /**
-     * @var Validate
-     */
+
     private $_validator;
-    /**
-     * @var Database
-     */
+
     private $_db;
-    /**
-     * ICollectController constructor.
-     */
-    public function __construct()
+    function __construct($f3)
     {
-        //Instantiate Fat-Free
-        $this->_f3 = Base::instance();
-        $this->_validator = new Validate();
-        $this->_db = new Database();
-        //Turn on Fat-Free error reporting
-        $this->_f3->set('DEBUG', 3);
+        $this->_f3 = $f3;
     }
 
-    /**
-     * Home page of iCollect
-     */
+
+
     public function home()
     {
         $view = new Template();
--- index.php
+++ index.php
@@ -4,12 +4,19 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 //add any requires here
+require_once('vendor/autoload.php');
 
 session_start();
 
 $controller = new Controller();
-
-$controller->getF3()->route("GET /", function (){
+//        //Instantiate Fat-Free
+        $this->_f3 = Base::instance();
+//        $this->_validator = new Validate();
+//        $this->_db = new Database();
+//        //Turn on Fat-Free error reporting
+//        $this->_f3->set('DEBUG', 3);
+//    }
+$controller->route("GET /", function (){
     global $controller;
     $controller ->home();
 });
